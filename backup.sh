41532ded9253f19b763b7dfa1e38a6ffa59a7f40
Nicolas Oudard <noudard@weborama.com>
2018-10-24 15:47:11 +0200

    Save backup script with download wp source

--- backup.sh
+++ backup.sh
@@ -12,3 +12,12 @@ ssh nicolas@oudard.org "tar cfz blog.oudard.org-$date.tar.gz production/wordpres
 scp nicolas@oudard.org:"blog.oudard.org-$date.tar.gz" .
 ssh nicolas@oudard.org "rm blog.oudard.org-$date.tar.gz"
 
+#
+#
+# After run ./backup.sh 
+tar xfz blog.oudard.org-$date.tar.gz
+mv blog-oudard-org-wp/wp-config.php .
+rm blog-oudard-org-wp
+mv production/wordpress blog-oudard-org-wp
+mv wp-config.php blog-oudard-org-wp
+rm -rf production "blog.oudard.org-$date.tar.gz"
